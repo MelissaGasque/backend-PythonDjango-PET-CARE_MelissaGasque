4916d8b7b2ee5444197ce6dbcd944dd9478c1e8d
Melissa Gasque <melissagasque.com>
2023-10-04 10:53:33 -0300

    feat: tarefa 1 finalizada

---
+++ pets/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class SexChoices(models.TextChoices):
+    NOT_INFORMED = "Not Informed",
+    MALE = "Male",
+    FEMALE = "Female"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20,
+        choices=SexChoices.choices,
+        default=SexChoices.NOT_INFORMED
+    )
+    group = models.ForeignKey(
+        "groups.Group",
+        related_name="pets",
+        on_delete=models.PROTECT,
+    )
+    traits = models.ManyToManyField(
+        "traits.Trait"
+    )
+
+    def __repr__(self) -> str:
+        return f"<Pet: {self.name}>"
